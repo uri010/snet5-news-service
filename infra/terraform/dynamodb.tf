a39f50a1391531fb1974d4f4a38b6cade6cf7e01
uri010 <dbfl0461@gmail.com>
2025-08-21 17:26:37 +0900

    DynamoDB 설정 추가

---
+++ infra/terraform/dynamodb.tf
@@ -0,0 +1,18 @@
+# 기존 DynamoDB 테이블 정보 가져오기
+data "aws_dynamodb_table" "naver_news_articles" {
+  name = "naver_news_articles"
+}
+
+# DynamoDB VPC Endpoint (Gateway Type - 무료)
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id          = aws_vpc.main.id
+  service_name    = "com.amazonaws.${var.region}.dynamodb"
+  route_table_ids = [aws_route_table.private.id]
+
+  vpc_endpoint_type = "Gateway"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-dynamodb-endpoint"
+    Type = "VPCEndpoint"
+  }
+}
