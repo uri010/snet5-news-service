b219cb290bdf13fd3b74a5483d04fa66b8f3e0aa
uri010 <dbfl0461@gmail.com>
2025-09-10 14:24:49 +0900

    필요 없는 부분 삭제

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -56,10 +56,4 @@ data "aws_caller_identity" "current" {}
 # 현재 region 정보
 data "aws_region" "current" {}
 
-# EKS 서비스 계정을 위한 OIDC thumbprint
-# (EKS 클러스터 생성 후 별도 파일에서 정의할 예정)
-# data "tls_certificate" "eks" {
-#   depends_on = [aws_eks_cluster.main]
-#   url        = aws_eks_cluster.main.identity[0].oidc[0].issuer
-# }
 
